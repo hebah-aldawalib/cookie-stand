cf4bedfc5fac2c3229cd1e0bf6300008e63dd163
hebah al daealieb <hebah20082gmail.com>
2021-07-11 15:57:15 +0300

    cooki

---
+++ app.js
@@ -0,0 +1,60 @@
+
+let Seattle={
+    name : "Seattle", 
+    mincus : 23 ,
+    maxcus : 65 ,
+    AvgCookie : 6.3 ,
+randmNumberCus : []
+
+function getRandomInt(max) {
+
+    return Math.floor(Math.random() * max);
+  }
+  
+  console.log(getRandomInt());
+ 
+  
+  console.log(getRandomInt());
+ 
+  
+  console.log(Math.random());
+  
+
+}
+
+
+let Tokyo= {
+    name : "Tokyo", 
+    mincus :3 ,
+    maxcus : 24 ,
+    AvgCookie : 1.4 ,
+randmNumberCus : []
+}
+
+
+let Dubai= {
+    name : "Dubai", 
+    mincus :11 ,
+    maxcus : 38 ,
+    AvgCookie : 3.7 ,
+randmNumberCus : []
+}
+
+
+let Paris= {
+    name : "Paris", 
+    mincus :20 ,
+    maxcus : 38 ,
+    AvgCookie :2.3 ,
+randmNumberCus : []
+}
+
+
+let Lima= {
+    name : "Lima", 
+    mincus :2 ,
+    maxcus : 16 ,
+    AvgCookie :4.6  ,
+randmNumberCus : []
+}
+
