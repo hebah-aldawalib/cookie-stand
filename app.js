9f9ff760cd6faef241711dbc2d064edf80efa689
hebah al daealieb <hebah20082gmail.com>
2021-07-25 01:37:22 +0300

    edit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// i was helped from someone.
+
+
 let hours = ['6 Am', '7 Am', '8 Am', '9 Am', '10 Am', '11 Am', '12 Pm', '1 Pm', '2 Pm', '3 Pm', '4 Pm', '5 Pm', '6 Pm', '7 Pm'];
 
  
